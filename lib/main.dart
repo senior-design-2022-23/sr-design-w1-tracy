99b1aa7237d45dd5847704259b0364b889900df8
Andre Battle <battleal@rose-hulman.edu>
2022-10-19 15:27:19 -0400

    Initial login page UI (Welcome, Sign In, Sign Up pages)

--- lib/main.dart
+++ lib/main.dart
@@ -5,111 +5,317 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Migraine App',
+      home: SignUpPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+            Color.fromARGB(255, 124, 154, 88),
+            Color.fromARGB(255, 141, 184, 86),
+            Color.fromARGB(255, 101, 120, 78),
+            Color.fromARGB(255, 109, 144, 67)
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          padding: const EdgeInsets.only(top: 100, left: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Welcome!',
+                style: TextStyle(
+                    fontSize: 50, color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              const Text(
+                'To begin tracking migraines login below',
+                style: TextStyle(
+                    fontSize: 16, color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 325),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      side: const BorderSide(
+                          width: 1, color: Color.fromARGB(255, 255, 255, 255)),
+                      minimumSize: const Size(330, 70),
+                    ),
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        minimumSize: const Size(330, 70),
+                        backgroundColor:
+                            const Color.fromARGB(255, 255, 255, 255)),
+                    child: const Text(
+                      "Sign In",
+                      style: TextStyle(
+                          fontSize: 17, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+            Color.fromARGB(255, 124, 154, 88),
+            Color.fromARGB(255, 141, 184, 86),
+            Color.fromARGB(255, 101, 120, 78),
+            Color.fromARGB(255, 109, 144, 67)
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          padding: const EdgeInsets.only(top: 100, left: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                tooltip: 'Back',
+                onPressed: () {},
+              ),
+              const Text(
+                'Sign In',
+                style: TextStyle(
+                    fontSize: 50, color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelStyle:
+                          TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      labelText: 'Username',
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelStyle:
+                          TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      labelText: 'Password',
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 70),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      side: const BorderSide(
+                          width: 1, color: Color.fromARGB(255, 255, 255, 255)),
+                      minimumSize: const Size(330, 70),
+                    ),
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        minimumSize: const Size(330, 70),
+                        backgroundColor:
+                            const Color.fromARGB(255, 223, 80, 80)),
+                    child: const Text(
+                      "Sign In With Google",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        minimumSize: const Size(330, 70),
+                        backgroundColor:
+                            const Color.fromARGB(255, 255, 255, 255)),
+                    child: const Text(
+                      "Create New Account",
+                      style: TextStyle(
+                          fontSize: 17, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+            Color.fromARGB(255, 124, 154, 88),
+            Color.fromARGB(255, 141, 184, 86),
+            Color.fromARGB(255, 101, 120, 78),
+            Color.fromARGB(255, 109, 144, 67)
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          padding: const EdgeInsets.only(top: 100, left: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                tooltip: 'Back',
+                onPressed: () {},
+              ),
+              const Text(
+                'Sign Up',
+                style: TextStyle(
+                    fontSize: 50, color: Color.fromARGB(255, 255, 255, 255)),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: Row(children: <Widget>[
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelStyle: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255)),
+                        labelText: 'First Name',
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelStyle: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255)),
+                        labelText: 'Last Name',
+                      ),
+                    ),
+                  ])),
+              Container(
+                  margin: const EdgeInsets.only(top: 50),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelStyle:
+                          TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      labelText: 'Username',
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelStyle:
+                          TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                      labelText: 'Password',
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 70),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      side: const BorderSide(
+                          width: 1, color: Color.fromARGB(255, 255, 255, 255)),
+                      minimumSize: const Size(330, 70),
+                    ),
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        minimumSize: const Size(330, 70),
+                        backgroundColor:
+                            const Color.fromARGB(255, 223, 80, 80)),
+                    child: const Text(
+                      "Sign In With Google",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        minimumSize: const Size(330, 70),
+                        backgroundColor:
+                            const Color.fromARGB(255, 255, 255, 255)),
+                    child: const Text(
+                      "Create New Account",
+                      style: TextStyle(
+                          fontSize: 17, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
