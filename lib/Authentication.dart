72823a5d045ec7ecd6cd96604f509fe4e5e23097
rhit-stiebysd <78181264+rhit-stiebysd@users.noreply.github.com>
2022-10-25 01:17:33 -0400

    signing in with google works

---
+++ lib/Authentication.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+Future<String> registerUserByEmail(String username, String email, String pass,
+    String first, String last) async {
+  final user = ParseUser.createUser(username, pass, email);
+  user.set("firstName", first);
+  user.set("lastName", last);
+  var response = await user.signUp();
+  if (response.success) {
+    return "Success!";
+  } else {
+    return response.error!.message;
+  }
+}
+
+Future<String> signUpGoogle(String first, String last) async {
+  final GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email']);
+  try {
+    await googleSignIn.signIn();
+  } catch (error) {
+    return error.toString();
+  }
+  if (await googleSignIn.isSignedIn()) {
+    final user = ParseUser.createUser(googleSignIn.currentUser?.email,
+        googleSignIn.currentUser?.id, googleSignIn.currentUser?.email);
+    user.set("firstName", first);
+    user.set("lastName", last);
+    var response = await user.signUp();
+    if (response.success) {
+      return "Success signing up with Google";
+    } else {
+      return response.error!.message;
+    }
+  }
+  return "Unable to sign up with Google.";
+}
