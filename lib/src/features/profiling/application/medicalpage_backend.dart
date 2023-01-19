9768352065fa91aab9eba172e78304c6d98fd9ba
rhit-stiebysd <78181264+rhit-stiebysd@users.noreply.github.com>
2023-01-18 23:15:12 -0500

    should be done, just needs testing and linking

---
+++ lib/src/features/profiling/application/medicalpage_backend.dart
@@ -0,0 +1,23 @@
+import 'package:migraine_aid/src/features/login/application/login_authentication.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import '../../../shared/userFunctions.dart';
+
+Future<bool> storeMedicalHistory(medText) async {
+  //TODO: remove next line when pages linked.
+  await doUserLogin('kbhuwalk@gmail.com', 'Kush1234');
+  ParseUser? user = await getCurrentUser();
+  if (user == null) {
+    return false;
+  }
+  QueryBuilder<ParseObject> queryUsers =
+      QueryBuilder<ParseObject>(ParseObject('UserInfo'))
+        ..whereEqualTo('UserObj', user.objectId);
+  final ParseResponse parseResponse = await queryUsers.query();
+  if (parseResponse.success && parseResponse.results != null) {
+    var obj = (parseResponse.results!.first) as ParseObject;
+    obj.set('MedicalHistory', medText);
+    await obj.save();
+    return true;
+  }
+  return false;
+}
