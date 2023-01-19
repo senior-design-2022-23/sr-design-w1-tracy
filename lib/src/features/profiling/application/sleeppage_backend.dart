facff59a5a74cd2b18a139f7304ea5861c79b7b4
rhit-bhuwalk <bhuwalk@rose-hulman.edu>
2023-01-18 19:20:16 -0500

    storesleepHours working, not tested

--- lib/src/features/profiling/application/sleeppage_backend.dart
+++ lib/src/features/profiling/application/sleeppage_backend.dart
@@ -3,17 +3,27 @@
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 import '../../../shared/userFunctions.dart';
+import '../../login/application/login_authentication.dart';
 
-Future storeSleepHours(hours) async {
-   ParseUser? user = await getCurrentUser();
-    if(user != null) {
-      var obj = ParseObject('UserInfo')
-        ..set('UserObj', user.objectId)
-        ..set('SleepInfo', hours);
-      await obj.save();
-    }
+Future<bool> storeSleepHours(hours) async {
+
+  //TODO: this line is for testing purposes. Remove when pages linked.
+  await doUserLogin('kbhuwalk@gmail.com', 'Kush1234');
+
+  ParseUser? user = await getCurrentUser();
+  if(user == null) {
+    return false;
   }
-
-
+  QueryBuilder<ParseObject> queryUsers = QueryBuilder<ParseObject>(ParseObject('UserInfo'))
+    ..whereEqualTo('UserObj', user.objectId);
+  final ParseResponse parseResponse = await queryUsers.query();
+  if (parseResponse.success && parseResponse.results != null) {
+    var obj = (parseResponse.results!.first) as ParseObject;
+      obj.set('SleepInfo', hours);
+    await obj.save();
+    return true;
+  }
+  return false;
+}
 
 
