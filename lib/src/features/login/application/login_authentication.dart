5ad33648e440880e357222fc06e27f63366126c3
rhit-stiebysd <78181264+rhit-stiebysd@users.noreply.github.com>
2023-01-24 21:58:00 -0500

    google sign in/registration should be working

--- lib/src/features/login/application/login_authentication.dart
+++ lib/src/features/login/application/login_authentication.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 import '../../login/presentation/login.dart';
@@ -7,4 +7,40 @@ import '../../login/presentation/login.dart';
 Future doUserLogin(username, password) async {
   final user = ParseUser(username, password, null);
   await user.login();
+}
+
+Future<String> registerUserByGoogle(String first, String last) async {
+  final GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email']);
+  try {
+    await googleSignIn.signIn();
+  } catch (error) {
+    return error.toString();
+  }
+  if (await googleSignIn.isSignedIn()) {
+    final user = ParseUser.createUser(googleSignIn.currentUser?.displayName,
+        googleSignIn.currentUser?.id, googleSignIn.currentUser?.email);
+    user.set("firstName", first);
+    user.set("lastName", last);
+    var response = await user.signUp();
+    if (response.success) {
+      return "Success!";
+    } else {
+      return response.error!.message;
+    }
+  }
+  return "Unable to sign up with Google.";
+}
+
+Future<String> loginWithGoogle(String first, String last) async {
+  final GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email']);
+  try {
+    await googleSignIn.signIn();
+  } catch (error) {
+    return error.toString();
+  }
+  if (await googleSignIn.isSignedIn()) {
+    await doUserLogin(
+        googleSignIn.currentUser?.displayName, googleSignIn.currentUser?.id);
+  }
+  return "Unable to sign up with Google.";
 }
