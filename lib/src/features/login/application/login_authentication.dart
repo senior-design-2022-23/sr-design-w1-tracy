e46b204206772edccaa7b68f9bceb87291786f8b
Sam Stieby <stiebysd@rose-hulman.edu>
2023-02-01 03:00:09 -0500

    added client and server ID. still didn't work.

--- lib/src/features/login/application/login_authentication.dart
+++ lib/src/features/login/application/login_authentication.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
@@ -9,12 +8,16 @@ Future doUserLogin(username, password) async {
 }
 
 Future<String> loginWithGoogle() async {
-  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: ['email'],
+      serverClientId: 'GYMIP6tfliC7C4s2HpUouH1MQkffo6WvXCnDu7uQ',
+      clientId: '11511470133-uv8gt8n2645mg41ttp2igeqso1550ckr.apps.googleusercontent.com');
   try {
-    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
-    await doUserLogin(googleUser?.displayName, googleUser?.id);
+    await googleSignIn.signIn();
+    print("did");
+    final user = ParseUser(googleSignIn.currentUser?.displayName, googleSignIn.currentUser?.id, null);
+    print(user.username);
+    await user.login();
   } catch (error) {
     return error.toString();
   }
